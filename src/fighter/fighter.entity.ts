7511ee0acf5ead1c72e080cf94ab0ca65047a744
dibadull <dinmukhammed.ibadulla@nu.edu.kz>
2025-05-15 19:04:30 +0500

    feat: add Fighter entity

---
+++ src/fighter/fighter.entity.ts
@@ -0,0 +1,46 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+
+@ObjectType()
+@Entity('fighters')
+export class Fighter {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column({ length: 100 })
+  firstName: string;
+
+  @Field()
+  @Column({ length: 100 })
+  lastName: string;
+
+  @Field({ nullable: true })
+  @Column({ length: 100, nullable: true })
+  nickname?: string;
+
+  @Field(() => Int)
+  @Column()
+  age: number;
+
+  @Field()
+  @Column({ length: 50 })
+  weightClass: string;
+
+  @Field(() => Int)
+  @Column({ default: 0 })
+  wins: number;
+
+  @Field(() => Int)
+  @Column({ default: 0 })
+  losses: number;
+
+  @Field(() => Int)
+  @Column({ default: 0 })
+  draws: number;
+
+  @Field({ nullable: true })
+  @Column({ length: 255, nullable: true })
+  imageUrl?: string;
+}
