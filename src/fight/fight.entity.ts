8a7ade84b1b91f80fc3e68b672e2fe2355b2c1d9
dibadull <dinmukhammed.ibadulla@nu.edu.kz>
2025-05-15 19:04:30 +0500

    feat: add Fight entity

---
+++ src/fight/fight.entity.ts
@@ -0,0 +1,44 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Fighter } from '../fighter/fighter.entity';
+import { Event } from '../event/event.entity';
+
+@ObjectType()
+@Entity('fights')
+export class Fight {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field(() => Event)
+  @ManyToOne(() => Event)
+  @JoinColumn({ name: 'event_id' })
+  event: Event;
+
+  @Field(() => Fighter)
+  @ManyToOne(() => Fighter)
+  @JoinColumn({ name: 'fighter_a_id' })
+  fighterA: Fighter;
+
+  @Field(() => Fighter)
+  @ManyToOne(() => Fighter)
+  @JoinColumn({ name: 'fighter_b_id' })
+  fighterB: Fighter;
+
+  @Field(() => Fighter, { nullable: true })
+  @ManyToOne(() => Fighter, { nullable: true })
+  @JoinColumn({ name: 'winner_id' })
+  winner?: Fighter;
+
+  @Field({ nullable: true })
+  @Column({ nullable: true })
+  method?: string;
+
+  @Field(() => Int, { nullable: true })
+  @Column({ nullable: true })
+  rounds?: number;
+
+  @Field({ nullable: true })
+  @Column({ nullable: true })
+  duration?: string;
+}
