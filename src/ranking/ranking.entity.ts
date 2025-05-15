eaa2d59bcb0849b66c902f02fa56396ec4967e46
dibadull <dinmukhammed.ibadulla@nu.edu.kz>
2025-05-15 19:04:30 +0500

    feat: add Ranking entity

---
+++ src/ranking/ranking.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Fighter } from '../fighter/fighter.entity';
+
+@ObjectType()
+@Entity('rankings')
+export class Ranking {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field(() => Fighter)
+  @ManyToOne(() => Fighter)
+  @JoinColumn({ name: 'fighter_id' })
+  fighter: Fighter;
+
+  @Field()
+  @Column({ length: 50 })
+  weightClass: string;
+
+  @Field(() => Int)
+  @Column({ default: 0 })
+  points: number;
+
+  @Field(() => Int, { nullable: true })
+  @Column({ nullable: true })
+  rankPosition?: number;
+}
