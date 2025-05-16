95b6a419f527c53686b27d19e850effa53f2f5d9
dibadull <dinmukhammed.ibadulla@nu.edu.kz>
2025-05-16 22:23:42 +0500

    feat: trigger ranking recalculation after fighter creation and update

--- src/fighter/fighter.module.ts
+++ src/fighter/fighter.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Fighter } from './fighter.entity';
 import { FighterService } from './fighter.service';
 import { FighterResolver } from './fighter.resolver';
+import { RankingCalculatorModule } from '../shared/utils/ranking-calculator.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Fighter])],
+  imports: [TypeOrmModule.forFeature([Fighter]),RankingCalculatorModule],
+  
   providers: [FighterService, FighterResolver],
 })
 export class FighterModule {}
--- src/fighter/fighter.service.ts
+++ src/fighter/fighter.service.ts
@@ -4,17 +4,23 @@ import { Repository } from 'typeorm';
 import { Fighter } from './fighter.entity';
 import { CreateFighterInput } from './fighter.dto';
 import { UpdateFighterInput } from './fighter.dto';
+import { RankingCalculatorService } from '../shared/utils/ranking-calculator.service';
 
 @Injectable()
 export class FighterService {
   constructor(
     @InjectRepository(Fighter)
     private readonly fighterRepo: Repository<Fighter>,
+    private readonly rankingCalculator: RankingCalculatorService,
   ) {}
 
   async create(data: CreateFighterInput): Promise<Fighter> {
     const fighter = this.fighterRepo.create(data);
-    return this.fighterRepo.save(fighter);
+    const savedFighter = await this.fighterRepo.save(fighter);
+
+    await this.rankingCalculator.recalculateRankings(savedFighter.weightClass);
+
+  return savedFighter;
   }
 
   async findAll(): Promise<Fighter[]> {
@@ -23,10 +29,13 @@ export class FighterService {
 
   async update(data: UpdateFighterInput): Promise<Fighter> {
     const fighter = await this.fighterRepo.preload(data);
-    if (!fighter) {
-      throw new Error(`Fighter with ID ${data.id} not found`);
-    }
-    return this.fighterRepo.save(fighter);
+    if (!fighter) throw new Error(`Fighter with ID ${data.id} not found`);
+
+    const updated = await this.fighterRepo.save(fighter);
+
+    await this.rankingCalculator.recalculateRankings(updated.weightClass);
+
+  return updated;
   }
   
   async remove(id: number): Promise<boolean> {
